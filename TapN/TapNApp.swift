d7974b03cefd55917ca2f488cc9eb37d2ff47bde
pjs9797 <1997pjs@naver.com>
2024-03-11 17:56:27 +0900

    Initial Commit

---
+++ TapN/TapNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapNApp.swift
+//  TapN
+//
+//  Created by 박중선 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TapNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
